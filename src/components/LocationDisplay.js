234bc4c1768d0a37340faf64f0f2fd49714b9f69
GitsOfVivek <singhvivek1999@yahoo.com>
2023-03-16 12:52:13 +0530

    //

--- src/components/LocationDisplay.js
+++ src/components/LocationDisplay.js
@@ -3,7 +3,7 @@ import { useLocation } from 'react-router-dom';
 
 function LocationDisplay() {
 	const { pathname } = useLocation();
-	return <div data-testid="location-display">{'/some-route'}</div>;
+	return <div data-testid="location-display">{pathname}</div>;
 }
 
 export default LocationDisplay;
