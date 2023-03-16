a0d973c772a2c3e001783a38af2aa32bfe64b291
GitsOfVivek <singhvivek1999@yahoo.com>
2023-03-16 11:05:46 +0530

    test id added

--- src/components/LocationDisplay .js
+++ src/components/LocationDisplay .js
@@ -3,7 +3,7 @@ import { useLocation } from 'react-router-dom';
 
 function LocationDisplay() {
 	const location = useLocation();
-	return <div>{location.pathname}</div>;
+	return <div data-testid="location-display">{location.pathname}</div>;
 }
 
 export default LocationDisplay;
