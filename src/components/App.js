bda93e5e984b1af6c05c9f5d29452053081d46a9
GitsOfVivek <singhvivek1999@yahoo.com>
2023-03-16 11:04:41 +0530

    done

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,34 @@
-import React, {Component, useState} from "react";
+import React, { Component, useState } from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import '../styles/App.css';
+import Navigation from './Navigation';
+import Home from './Home';
+import About from './About';
+import NoMatch from './NoMatch';
+import LocationDisplay from './LocationDisplay ';
+
+const AppRouter = () => {
+	return (
+		<BrowserRouter>
+			<Navigation />
+			<Switch>
+				<Route exact path={'/'} component={Home} />
+				<Route path={'/about'} component={About} />
+				<Route path={'*'} component={NoMatch} />
+			</Switch>
+			<LocationDisplay />
+		</BrowserRouter>
+	);
+};
 
 class App extends Component {
-    render() {
-
-        return(
-            <div id="main">
-               {/* Do not remove the main div */}
-            </div>
-        )
-    }
+	render() {
+		return (
+			<div id="main">
+				<AppRouter />
+			</div>
+		);
+	}
 }
-
 
 export default App;
---
+++ src/components/LocationDisplay .js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { useLocation } from 'react-router-dom';
+
+function LocationDisplay() {
+	const location = useLocation();
+	return <div>{location.pathname}</div>;
+}
+
+export default LocationDisplay;
